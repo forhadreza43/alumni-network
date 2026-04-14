e62a3ad86e35991433c868231b1c541bdb136280
Reza <forhad.bimt@gmail.com>
2026-04-14 21:06:41 +0600

    added-nav-main

---
+++ components/nav-main.tsx
@@ -0,0 +1,50 @@
+"use client"
+
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar"
+import { cn } from "@/lib/utils"
+import { CirclePlusIcon, MailIcon } from "lucide-react"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+export function NavMain({
+  items,
+}: {
+  items: {
+    title: string
+    url: string
+    icon?: React.ReactNode
+  }[]
+}) {
+  const pathname = usePathname()
+  console.log(pathname);
+  return (
+    <SidebarGroup>
+      <SidebarGroupContent className="flex flex-col gap-2">
+        <SidebarMenu>
+          {items.map((item) => (
+            <SidebarMenuItem key={item.title}>
+              <Link href={item.url}>
+                <SidebarMenuButton
+                  tooltip={item.title}
+                  className={cn({
+                    "min-w-8 bg-primary text-primary-foreground duration-200 ease-linear hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground":
+                      pathname === item.url,
+                  })}
+                >
+                  {item.icon}
+                  <span>{item.title}</span>
+                </SidebarMenuButton>
+              </Link>
+            </SidebarMenuItem>
+          ))}
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  )
+}
